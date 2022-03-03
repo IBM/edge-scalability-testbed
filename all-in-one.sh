4d24dfe66d4014610e722d9ba5ab18213ba9f35e
Jun Duan <jun.duan@ibm.com>
2022-03-03 15:53:55 -0500

    cleaning up

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,4 +1,3 @@
-# tested
 ansible-playbook create-ec2.yaml -e "cluster_name=hub"
 ansible-playbook create-ec2.yaml -e "cluster_name=managed masters=1 workers=5"
 ansible-playbook -i .data/hosts_hub deploy-master.yaml
@@ -11,5 +10,3 @@ ansible-playbook -i .data/hosts_hub create-vhub.yaml -e "vhub_name=vks1"
 ansible-playbook -i .data/hosts_managed deploy-mockclusters.yaml 
 ansible-playbook -i .data/hosts_managed scale-mockclusters.yaml -e "num_replicas=4"
 ansible-playbook -i .data/hosts_hub vhub-approveCSR.yaml 
-
-# work in progress
